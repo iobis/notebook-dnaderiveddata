0134dcea7dddb61bae1edf400f50f89c66c6a1a0
SSuominen1 <s.suominen@unesco.org>
2021-10-19 15:07:03 +0200

    Change to alignment with mafft

--- script.R
+++ script.R
@@ -1,8 +1,10 @@
 library(dplyr)
 library(robis)
 library(ape)
-library(msa)
+library(ips)
 library(dendextend)
+library(ggplot2)
+library(ggtree)
 
 # get data from OBIS
 
@@ -21,23 +23,42 @@ dna <- dna_records(occ, fields = c("id", "scientificName", "phylum", "class", "o
 
 dna_subset <- dna[sample(1:nrow(dna), 150),]
 
-# align with clustal
-
-aligned <- msa::msa(dna_subset$DNA_sequence, type = "dna")
 
 # convert to ape DNAbin
 
-sequences <- t(sapply(strsplit(as.character(aligned), ""), tolower))
+sequences <- t(sapply(strsplit(as.character(dna_subset), ""), tolower))
 rownames(sequences) <- paste0(substr(dna_subset$id, 1, 5), " - ", dna_subset$phylum)
 sequences <- ape::as.DNAbin(sequences)
 sequences
 
+#align with mafft
+
+ali_mafft=ips::mafft(sequences, method = "auto")
+
+
 # dist.dna and hclust
 
-distances <- ape::dist.dna(sequences, model = "raw")
+distances <- ape::dist.dna(ali_mafft, model = "raw")
 h_cluster <- hclust(distances, method = "average")
 
 dend <- as.dendrogram(h_cluster) %>% set("labels_cex", 0.6)
 dend <- dend %>% set("labels_col", as.numeric(as.factor(substr(labels(dend), 9, 100))))
 
 plot(dend)
+
+#For flexible phylogenetic tree visualization, can build on this new package:
+
+tree<-njs(distances)
+ggt<-ggtree(tree, cex = 0.8, aes(color=branch.length))+geom_tiplab(size=2)
+ggt
+
+#Also bootstrapping
+
+#Bootstrapping
+myBoots <- boot.phylo(tree, ali_mafft, function(e) njs(dist.dna(e, model = "TN93")))
+bp2 <- tibble(node=1:Nnode(tree) + Ntip(tree), bootstrap = myBoots)
+bptree <- full_join(tree, bp2, by="node")
+
+ggtree(bptree) +
+  geom_tiplab(size=2) +
+  geom_nodelab(aes(label=bootstrap), size=2)
